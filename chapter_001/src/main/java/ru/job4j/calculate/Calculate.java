a96a131a05d68764ae3d641ca015d67945cebb28
Evgenii Shegai <34shegai@gmail.com>
2018-10-23 16:01:09 +0900

    JavaDoc.Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j.calculate;
 
+/**
+  * Calculate
+  *@author Evgenii Shegai(34shegai@gmail.com)
+*/
+
 public class Calculate {
+	
+	/**
+	 * Main
+	 *@param args - args
+	*/
 	
 	public static void main(String[]args) {
 		System.out.print("Hello World");
