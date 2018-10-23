25e8c8d2fb1d6ff61c0888f7df0f07a9a93d1f20
Evgenii Shegai <34shegai@gmail.com>
2018-10-23 17:35:05 +0900

    1.4 JUnit

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Evgenii Shegai (34shegai@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/
+@Test
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Evgenii Shegai";
+    String expect = "Echo, echo, echo : Evgenii Shegai";
+    assertThat(result, is(expect));
+}
+ 
+}
