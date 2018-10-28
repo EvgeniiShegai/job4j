1308546e53bdaf9cb0c69030fd62f698bc529892
Evgenii Shegai <34shegai@gmail.com>
2018-10-28 22:19:40 +0900

    Подсчет суммы четных чисел в диапозоне

--- chapter_001/src/main/java/ru/job4j/calculate/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/calculate/loop/Counter.java
@@ -4,7 +4,7 @@ public class Counter {
 
     public int add(int start, int finish) {
         int res = 0;
-        for (int i = start; i <= 10; i++) {
+        for (int i = start; i <= finish; i++) {
             if (i % 2 == 0) {
                 res += i;
             }
