0b61963824f8e1e63f795db23e122fe477287bfd
Evgenii Shegai <34shegai@gmail.com>
2018-11-08 14:37:19 +0900

    Измененный pom.xml родительский

--- chapter_001/src/main/java/ru/job4j/calculate/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/calculate/array/MatrixCheck.java
@@ -7,8 +7,10 @@ public class MatrixCheck {
         for (int i = 0; i < array.length - 1; i++) {
             if (array[i][i] != array[i + 1][i + 1]) {
                 res = false;
+                break;
             } else if (array[i][array.length - 1 - i] != array[i + 1][array.length - 2 - i]) {
                 res = false;
+                break;
             }
         }
         return res;
