cd861b2585535bb82406488e8f005caa0118dbad
Evgenii Shegai <34shegai@gmail.com>
2018-11-11 08:20:50 +0900

    Реализация профессий в коде

---
+++ chapter_002/src/main/java/ru/job4j/profession/Doctor.java
@@ -0,0 +1,9 @@
+package ru.job4j.profession;
+
+public class Doctor extends Profession {
+
+    public Pacient heal(Pacient temp) {
+
+        return temp;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/profession/Profession.java
@@ -0,0 +1,21 @@
+package ru.job4j.profession;
+
+/**
+ * Professions
+ * author Shegai Evgenii(34shegai@gmail.com)
+ */
+
+public class Profession {
+
+    public String name;
+    public String profession;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public String getProfession(){
+        return this.profession;
+    }
+
+}
